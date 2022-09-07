c90ab4772dead36c8f3a13c70912c326685ca7ca
KILLERAK-7 <shakthebarane@gmail.com>
2022-09-08 05:25:12 +0530

    Body parsing update post

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const { default: axios } = require('axios');
+const axios = require("axios")
 const express = require('express')
 const app = express()
 const port = 5000
 const request = require("request");
-
+const cors = require("cors")
+const bodyParser = require("body-parser")
 
 /*app.get('/', (req, res) => {
   res.send('Hello World!')
 })*/
 
-
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/getchat', async (req,res) => {
     let {q}= req.query;
@@ -22,11 +25,20 @@ app.get('/getchat', async (req,res) => {
         n += 1;
     });
     let count = Array(n);
-    //counts the no.of matchs per index
+    //counts the no.of matchs of to pattern per index
     data.intent.forEach((value,index) => {
         count[index] = 0;
         q.toString().split(" ").forEach(element => {
             value.pattern.forEach(key => {
+                if(element.toLowerCase() == key.toLowerCase())
+                    count[index] += 1;        
+            });
+        });
+    });
+    //counts the no.of matchs of to pattern per index
+    data.intent.forEach((value,index) => {
+        q.toString().split(" ").forEach(element => {
+            value.response.forEach(key => {
                 if(element.toLowerCase() == key.toLowerCase())
                     count[index] += 1;        
             });
@@ -45,17 +57,38 @@ app.get('/getchat', async (req,res) => {
             max = count[i];
             inx = i;
         }
-            
     }
+
     // Assigns the response to the ans variable
     data.intent.forEach((value,index) => {
         if(inx == index)
             ans = value.response[0];            
     });  
-    
+
     // Sends the response to the webpage
     console.log(ans);
     res.send(ans);
+
+});
+
+app.post('/getfeedback',async (req,res,next) => {
+    const fs= require("fs");
+    const data =await require("http://localhost:5001/data.json");
+    console.log(req.body);
+    let {question,feedback}= req.body;
+    q.toString().split(" ").forEach(element => {
+        let user = {pattern: element};
+        data.push(user);
+        fs.writeFile("data.json", JSON.stringify(users), err => {
+            if (err) throw err;
+                console.log("Done writing");
+            });
+    });
+    res.status(200)
+    next();
+})
+.get("/getfeedback",async (req,res) => {
+    res.send("OK 200");
 });
 
 
